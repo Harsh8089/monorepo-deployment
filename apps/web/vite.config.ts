526c65eaabb4ab0e04691ebe30b25fbe56aefd05
Harsh8089 <harshjain3575@gmail.com>
2026-05-23 16:08:37 +0530

    serving fe in prod using build

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const proxyConfig = {
+  '/api': {
+    target: process.env.BACKEND_URL || "http://backend:3000",
+    changeOrigin: true
+  }
+}
+
 export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-    proxy: {
-      '/api': {
-        target: "http://backend:3000",
-        changeOrigin: true
-      }
-    }
+    proxy: proxyConfig
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 5173,
+    proxy: proxyConfig
   }
 });
